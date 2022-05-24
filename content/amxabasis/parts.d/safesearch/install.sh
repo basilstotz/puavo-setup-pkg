6967339b523f4232e732d9e4edf1dac6b39390d2
Basil Stotz <stotz.basil@opinsys-q0AXi4JA>
2022-05-24 18:43:51 +0200

    *

---
+++ content/amxabasis/parts.d/safesearch/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+cp ./puavo-pkg-renew /usr/local/sbin/.
+
+
+cat <<EOF > /etc/systemd/system/puavo-safesearch.service
+[Unit]
+Description=Puavo Safesearch 
+#After=syslog.target network.target nss-lookup.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/sbin/puavo-safesearch
+
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable puavo-safesearch.service
+
+
+
+
+
--- content/virtualltsp/install.sh
+++ content/virtualltsp/install.sh
@@ -3,15 +3,12 @@
 cd $(dirname $0)
 
 cp ./tree/etc/avahi/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
-cp ./tree/etc/systemd/system/puavo-virtualltsp-setup.service /etc/systemd/system/puavo-virtualltsp-setup.service
-cp ./tree/etc/systemd/system/puavo-virtualltsp-daemon.service /etc/systemd/system/puavo-virtualltsp-daemon.service
+
+cp ./tree/usr/local/sbin/puavo-virtualltsp-client /usr/local/sbin/puavo-virtualltsp-client
+cp ./tree/etc/systemd/system/puavo-virtualltsp-client.service /etc/systemd/system/puavo-virtualltsp-client.service
 
 cp ./tree/usr/local/bin/puavo-find-puavobox /usr/local/bin/puavo-find-puavobox 
 
-cp ./tree/usr/local/sbin/puavo-virtualltsp-client /usr/local/sbin/puavo-virtualltsp-client
-cp ./tree/usr/local/sbin/puavo-virtualltsp-daemon /usr/local/sbin/puavo-virtualltsp-daemon
-cp ./tree/usr/local/sbin/puavo-virtualltsp-setup /usr/local/sbin/puavo-virtualltsp-setup
-
-systemctl enable puavo-virtualltsp-setup.service
+systemctl enable puavo-virtualltsp-client.service
 
 exit 0
